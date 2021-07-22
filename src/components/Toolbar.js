41dd7edfc1c162f41f2b9f47f24b6da2ead5b0da
sifawm <metaforaideas@gmail.com>
2021-07-22 18:59:50 +0200

    add box when click over button

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useRef } from 'react';
+import uuid from 'uuid/v4';
+import BoxModel from '../stores/models/Box';
 
-function Toolbar() {
+function Toolbar(props) {
+  const inputColor = useRef();
+
+  const addBox = () => {
+    const box = BoxModel.create({
+      id: uuid(),
+      color: inputColor.current.value,
+      left: 0,
+      top: 0,
+    });
+
+    props.store.addBox(box);
+  };
+
   return (
     <div className="toolbar">
-      <button>Add Box</button>
+      <button onClick={addBox}>Add Box</button>
       <button>Remove Box</button>
-      <input type="color" />
+      <input ref={inputColor} type="color" aria-label="color-input" />
       <span>No boxes selected</span>
     </div>
   );

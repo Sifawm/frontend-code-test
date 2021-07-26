41cb1e4a63475a5adf74ce4a466f72437c759840
sifawm <metaforaideas@gmail.com>
2021-07-26 10:33:13 +0200

    add drag tests

--- src/tests/components/BoxDraggable.test.js
+++ src/tests/components/BoxDraggable.test.js
@@ -2,6 +2,18 @@ import React from 'react';
 import BoxModel from '../../stores/models/Box';
 import BoxDraggable from '../../components/BoxDraggable';
 import { fireEvent, render } from '@testing-library/react';
+import interact from 'interactjs';
+import '../../stores/UndoManager';
+import { undoManager } from '../../stores/UndoManager';
+
+jest.mock('../../stores/UndoManager', () => {
+  return {
+    undoManager: {
+      startGroup: jest.fn(cb => cb()),
+      stopGroup: jest.fn(),
+    },
+  };
+});
 
 describe('BoxDraggable', () => {
   let box;
@@ -54,5 +66,54 @@ describe('BoxDraggable', () => {
 
     fireEvent.click(container.querySelector('#box-bowser'), { ctrlKey: true });
     expect(box.unselect).toBeCalled();
+  });
+
+  it('calls move when user drag box', async () => {
+    box.move = jest.fn();
+
+    const { container } = render(
+      <BoxDraggable id="box-bowser" selected={true} box={box}>
+        box
+      </BoxDraggable>
+    );
+
+    const boxElement = container.querySelector('#box-bowser');
+
+    interact(boxElement).fire({
+      type: 'dragstart',
+      target: boxElement,
+      pageX: 0,
+      pageY: 0,
+      clientX: 0,
+      clientY: 0,
+    });
+
+    expect(undoManager.startGroup).toBeCalled();
+
+    interact(boxElement).fire({
+      type: 'dragmove',
+      target: boxElement,
+      pageX: 0,
+      pageY: 0,
+      clientX: 0,
+      clientY: 0,
+      dx: 400,
+      dy: 200,
+    });
+
+    expect(box.move).toBeCalledWith(400, 200);
+
+    interact(boxElement).fire({
+      type: 'dragend',
+      target: boxElement,
+      pageX: 0,
+      pageY: 0,
+      clientX: 0,
+      clientY: 0,
+      dx: 400,
+      dy: 200,
+    });
+
+    expect(undoManager.stopGroup).toBeCalled();
   });
 });
